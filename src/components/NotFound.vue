fe29e448e95fb09bf91733dec0537b15ac7bb4ba
Ipaye Alameen <Akinkunmi007@gmail.com>
2018-08-25 04:16:16 +0100

    Add 404 page for all pages not done yet

---
+++ src/components/NotFound.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="notfound">
+    <Header></Header>
+    <section class="padding-small">
+      <div class="container">
+        <div class="row">
+          <div class="col-xl-8 col-lg-10">
+            <h2 class="text-superbig mt-5">404</h2>
+            <p class="lead">We don't know what what happened but that <strong>page is not here</strong>.</p>
+            <p class="lead">Please <a href="#" class="search text-bold">use search</a> or <a href="/" class="text-bold">start from the homepage</a>.</p>
+          </div>
+          <div class="col-xl-4">
+            <img src="../assets/notfound.svg" width="600px" alt="file not found">
+          </div>
+        </div>
+      </div>
+    </section>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/shared/Header';
+import Footer from '@/components/shared/Footer';
+
+export default {
+	components: {
+		Header,
+		Footer
+	},
+	name: 'NotFound'
+};
+</script>
+
