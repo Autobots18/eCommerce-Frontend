cee30e5cec027392353728bdf2d0167dc4bb8db6
Ipaye Alameen <Akinkunmi007@gmail.com>
2018-08-25 04:17:17 +0100

    Add routing for signin, signup and 404

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/components/Home';
+import SignIn from '@/components/SignIn';
+import SignUp from '@/components/SignUp';
+import NotFound from '@/components/NotFound';
 
 Vue.use(Router);
 
@@ -11,6 +14,21 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/signin',
+      name: 'Sign-in',
+      component: SignIn
+    },
+    {
+      path: '/signup',
+      name: 'Sign-up',
+      component: SignUp
+    },
+    {
+      path: '*',
+      name: '404',
+      component: NotFound
     }
   ]
 });
